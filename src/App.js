8296a462231b99826f4c2a13f706529cefb01c8a
Bubble Ninja <mr.slygrin@gmail.com>
2022-10-17 12:56:39 +0500

    second changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Body from './components/Body/Body';
+import Header from './components/Header/Header';
+
 function App() {
    return (
       <div className='App'>
-         <img src={require('./assets/1.jpg')} alt='pic' />
+         <Header />
+         <Body />
       </div>
    );
 }
---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import './body.css';
+
+const Body = () => {
+   return (
+      <div className='body'>
+         <div>Body</div>
+      </div>
+   );
+};
+
+export default Body;
