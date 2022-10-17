0eddeb81bfa614fac658176ab543ddfd2fb574e5
Bubble Ninja <mr.slygrin@gmail.com>
2022-10-17 23:13:29 +0500

    header finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,19 @@ import './header.css';
 const Header = () => {
    return (
       <div className='header'>
-         <div>Header</div>
+         <div className='header__text'>
+            <h1>Huge Collection of High Resolution Wallpapers</h1>
+         </div>
+         <div className='header__search-bar'>
+            <input type='text' placeholder='Search for images' />
+            <i
+               onClick={() => {
+                  console.log('hiiiii');
+               }}
+            >
+               <img src={require('../../assets/search-icon.png')} alt='' />
+            </i>
+         </div>
       </div>
    );
 };
